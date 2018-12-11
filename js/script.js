91376848a6b94292e96f4f12b884de09437e8ce1
wiliamtaesunglee <wiliam.tae@gmail.com>
2018-12-11 18:51:26 -0200

    counter firebase adicted

--- js/script.js
+++ js/script.js
@@ -1,44 +1,132 @@
 
-const userOff = document.querySelector('.status');
-const userImg = document.querySelector('.userPhoto a img');
-function onSignIn(googleUser) {
-    // Useful data for your client-side scripts:
-    
-    var profile = googleUser.getBasicProfile();
-    console.log("ID: " + profile.getId()); // Don't send this directly to your server!
-    console.log('Full Name: ' + profile.getName());
-    console.log('Given Name: ' + profile.getGivenName());
-    console.log('Family Name: ' + profile.getFamilyName());
-    console.log("Image URL: " + profile.getImageUrl());
-    console.log("Email: " + profile.getEmail());
-    
-    
-    
-    
-    // The ID token you need to pass to your backend:
-    var id_token = googleUser.getAuthResponse().id_token;
-    console.log("ID Token: " + id_token);
-    
-    const userImgUrl = profile.getImageUrl();
-    const menu = document.querySelector('.menu');
-    menu.children[0].innerHTML = profile.getGivenName();
-    menu.children[1].innerHTML = '<p>status</p>'
-    userOff.innerHTML = 'available';
-    userImg.setAttribute('src', userImgUrl);
-    
-};
 
-//! singOut
-const clearUser = document.querySelector('.name');
-function signOut() {
-    var auth2 = gapi.auth2.getAuthInstance();
-    auth2.signOut().then(function () {
-        console.log('User signed out.');
+const userOff = document.querySelector('.status');
+
+const db = firebase.firestore();
+const settings = {timestampsInSnapshots: true};
+db.settings(settings);
+
+// Ler
+// db.collection("teste").get().then(res => {
+//     res.forEach((doc) => {
+//         console.log(`${doc.id} => ${doc.data().name}`);
+//     });
+// })
+
+// Adicionar
+// db.collection("teste").doc("XPTO").set({
+//     name: "Hugo"
+// });
+
+// Atualizar
+// db.collection("teste").doc("XPTO").update({
+//     name: "Will"
+// });
+//! regras de autenticações
+const provider = new firebase.auth.GoogleAuthProvider();
+provider.addScope('https://www.googleapis.com/auth/contacts.readonly');
+firebase.auth().languageCode = 'pt';
+provider.setCustomParameters({
+    'login_hint': 'user@example.com'
+  })
+
+  firebase.auth().signInWithPopup(provider).then(function(result) {
+    // This gives you a Google Access Token. You can use it to access the Google API.
+    var token = result.credential.accessToken;
+    // The signed-in user info.
+    var user = result.user;
+    // ...
+  }).catch(function(error) {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    // The email of the user's account used.
+    var email = error.email;
+    // The firebase.auth.AuthCredential type that was used.
+    var credential = error.credential;
+    // ...
+  });
+
+  firebase.auth().getRedirectResult().then(function(result) {
+    if (result.credential) {
+        // This gives you a Google Access Token. You can use it to access the Google API.
+        var token = result.credential.accessToken;
+        // ...
+    }
+    // The signed-in user info.
+    var user = result.user;
+}).catch(function(error) {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    // The email of the user's account used.
+    var email = error.email;
+    // The firebase.auth.AuthCredential type that was used.
+    var credential = error.credential;
+    // ...
+});
+
+  firebase.auth().signOut().then(function() {
+      // Sign-out successful.
+    }).catch(function(error) {
+        // An error happened.
     });
-    clearUser.innerHTML = 'user';
-    userOff.innerHTML = 'off line';
-    userImg.setAttribute('src','http://placehold.jp/150x150.png');
-}
+    
+    //!dados usuários
+
+var user = firebase.auth().currentUser;
+var name, email, photoUrl, uid, emailVerified;
+var user = firebase.auth().currentUser;
+
+firebase.auth().onAuthStateChanged(function(user) {
+    if (user != null) {
+        user.providerData.forEach(function (profile) {
+            // const userImgUrl = profile.getImageUrl();
+            const userImg = document.querySelector('.userPhoto a img');
+            const menu = document.querySelector('.menu');
+            menu.children[0].innerHTML = profile.displayName;
+            // menu.children[1].innerHTML = '<p>status</p>'
+            // userOff.innerHTML = 'available';
+            userImg.setAttribute('src', profile.photoURL);
+
+            console.log("Sign-in provider: " + profile.providerId);
+            console.log("  Provider-specific UID: " + profile.uid);
+            console.log("  Name: " + profile.displayName);
+            console.log("  Email: " + profile.email);
+            console.log("  Photo URL: " + profile.photoURL);
+        });
+
+        // name = user.displayName;
+        // email = user.email;
+        // photoUrl = user.photoURL;
+        // emailVerified = user.emailVerified;
+        // uid = user.uid;  
+    } else {
+      console.log('logout');
+    }
+  });
+
+
+// function onSignIn(googleUser) {
+        
+    
+    
+    
+    
+    
+// };
+
+// //! singOut
+// const clearUser = document.querySelector('.name');
+// function signOut() {
+//     var auth2 = gapi.auth2.getAuthInstance();
+//     auth2.signOut().then(function () {
+//         console.log('User signed out.');
+//     });
+//     clearUser.innerHTML = 'user';
+//     userOff.innerHTML = 'off line';
+//     userImg.setAttribute('src','http://placehold.jp/150x150.png');
+// }
 
 //! tiemr function
 function startTimer(duration, display) {
@@ -107,6 +195,13 @@ function stop() {
 }
 
 parar.addEventListener('click', stop);
+
+function logado() {
+    if(onSignIn === true) {
+        console.log(profile.getName());
+    }
+}
+
 
 
 
